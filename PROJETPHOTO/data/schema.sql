2105218f4a91be465d4634ca9b2d000dd6e05cdd
LudovicUrbes <68041508+LudovicUrbes@users.noreply.github.com>
2022-11-16 16:17:50 +0100

    Add files via upload

--- PROJETPHOTO/data/schema.sql
+++ PROJETPHOTO/data/schema.sql
@@ -1,19 +1,16 @@
-drop table users;
+drop table utilisateur;
 drop table photo;
 
-CREATE TABLE users (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`username` VARCHAR(20) NOT NULL,
-	`password` VARCHAR(250) NOT NULL,
-	`role` VARCHAR(20) NOT NULL default "users",
-	PRIMARY KEY (`id`),
-	UNIQUE (`username`)
+CREATE TABLE utilisateur (
+	`id` INT AUTO_INCREMENT UNIQUE,
+	`nom` VARCHAR(20) NOT NULL,
+    `prenom` VARCHAR(20) NOT NULL,
+    `departement` VARCHAR(20) NOT NULL,
+    PRIMARY KEY (`id`),
 ) ENGINE=InnoDB;
 
-INSERT INTO `users` (`username`, `password`, `role`, `is_published`) VALUES ("root", "gtrnet", "admin", 0);
-
 CREATE TABLE photo (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`nom` TEXT NOT NULL,
+	`id` INT AUTO_INCREMENT UNIQUE,
+    `like` INT AUTO_INCREMENT 0,
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
