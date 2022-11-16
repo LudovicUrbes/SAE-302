ae7133eaf16fe801e289424cb81e9552f0985ede
LudovicUrbes <68041508+LudovicUrbes@users.noreply.github.com>
2022-11-16 17:26:04 +0100

    Update schema.sql

--- PROJETPHOTO/data/schema.sql
+++ PROJETPHOTO/data/schema.sql
@@ -2,8 +2,8 @@ drop table utilisateur;
 drop table photo;
 
 CREATE TABLE utilisateur (
-	`id` INT AUTO_INCREMENT UNIQUE,
-	`nom` VARCHAR(20) NOT NULL,
+    `id` INT AUTO_INCREMENT UNIQUE,
+    `nom` VARCHAR(20) NOT NULL,
     `prenom` VARCHAR(20) NOT NULL,
     `departement` VARCHAR(20) NOT NULL,
     `vote_possible` INT default 1,
@@ -14,8 +14,9 @@ CREATE TABLE utilisateur (
 INSERT INTO `utilisateur` (`nom`, `prenom`, `departement`) VALUES ("LEROUX", "Gauthier", "R&T");
 
 CREATE TABLE photo (
-	`id` INT AUTO_INCREMENT UNIQUE,
+    `id` INT AUTO_INCREMENT UNIQUE,
+    `stockage` NOT NULL,
     `legend` VARCHAR(100) NOT NULL,
     `like` INT default 0,
-	PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
