bebe62bd5551865de2ea76366a07c74837ae6b26
LudovicUrbes <68041508+LudovicUrbes@users.noreply.github.com>
2022-11-16 16:39:45 +0100

    Add files via upload

--- PROJETPHOTO/data/schema.sql
+++ PROJETPHOTO/data/schema.sql
@@ -6,11 +6,16 @@ CREATE TABLE utilisateur (
 	`nom` VARCHAR(20) NOT NULL,
     `prenom` VARCHAR(20) NOT NULL,
     `departement` VARCHAR(20) NOT NULL,
-    PRIMARY KEY (`id`),
+    `vote_possible` INT default 1,
+    `choix` INT NULL,
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
+
+INSERT INTO `utilisateur` (`nom`, `prenom`, `departement`) VALUES ("LEROUX", "Gauthier", "R&T");
 
 CREATE TABLE photo (
 	`id` INT AUTO_INCREMENT UNIQUE,
-    `like` INT AUTO_INCREMENT 0,
+    `legend` VARCHAR(100) NOT NULL,
+    `like` INT default 0,
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
