94b6570f5a526efe11c4f05e11e7bd3a2537df07
LudovicUrbes <ludovic.urbes@gmail.com>
2022-11-18 12:08:26 +0100

    3

--- PROJETPHOTO/html/index.php
+++ PROJETPHOTO/html/index.php
@@ -1,15 +1,5 @@
-<?php session_start(); 
-if($_SERVER['REQUEST_URI'] === "/login.php"){
-    #l'utilisateur souhaite accéder à la page de login, on le laisse faire
+<?php session_start(); ?>
 
-//else if(isset($_SESSION['logout']) &&  $_SESSION['logon'] === true){
-    # l'utilisateur demande une page différente de /login.php et il est authentifié
-    # on le laisse passer
-//else {
-    # sinon on le redirige vers /login.php et ON ARRÊTE l'exécution du script
-    //header('Location: /login.php');
-    //die();
-} ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -27,22 +17,19 @@ if($_SERVER['REQUEST_URI'] === "/login.php"){
 <meta http-equiv="expires" content="0">
 <link href="./CSS/app.css" rel="stylesheet" type="text/css">
 </head>
-<body>
+
 	<header>
 			<div id="nom_site">
 				<h1><i><b>Votez !</b></i></h1>
 			</div>
 	</header>
 
+
+
+<body>
+
 <section>
     <article id="img_inssert">
-		<?php
-			include ("transfert.php");
-			if ( isset($_FILES['fic']) )
-			{
-				transfert();
-			}
-		?>
 		<form method="post" enctype="multipart/form-data">
 			<div>
 				<label for="fic">Choose images to upload (PNG, JPG)</label>
@@ -128,58 +115,7 @@ function returnFileSize(number) {
   }
 }
 
-            </script>
-
-
-<?php
-    function transfert(){
-        $ret        = false;
-        $img_blob   = '';
-        $img_taille = 0;
-        $img_type   = '';
-        $img_nom    = '';
-        $taille_max = 250000;
-        $ret        = is_uploaded_file($_FILES['fic']['tmp_name']);
-        
-        if (!$ret) {
-            echo "Problème de transfert";
-            return false;
-        } else {
-            // Le fichier a bien été reçu
-            $img_taille = $_FILES['fic']['size'];
-            
-            if ($img_taille > $taille_max) {
-                echo "Trop gros !";
-                return false;
-            }
-
-            $img_type = $_FILES['fic']['type'];
-            $img_nom  = $_FILES['fic']['name'];
-        }
-		$img_type = $_FILES['fic']['type'];
-        $img_nom = $_FILES['fic']['name'];
-        include ("connexion.php");
-        $img_blob = file_get_contents ($_FILES['fic']['tmp_name']);
-        $req = "INSERT INTO images (" .
-                            "img_nom, img_taille, img_type, img_blob " .
-                            ") VALUES (" .
-                            "'" . $img_nom . "', " .
-                            "'" . $img_taille . "', " .
-                            "'" . $img_type . "', " .
-                            "'" . addslashes ($img_blob) . "') "; // N'oublions pas d'échapper le contenu binaire
-        $ret = mysql_query ($req) or die (mysql_error ());
-        return true;
-    }
-?>
-
-
-
-    </article>
-
-    <article>
-		<img src="./Images/Guitares/Corvin/3170066.JPG" width="350" height="525" alt="">
-	</article>
-
+</script>
 
 
 
