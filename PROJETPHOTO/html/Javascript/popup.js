60cc4007a785d425da6c776a7651c3322d3c2a51
LudovicUrbes <68041508+LudovicUrbes@users.noreply.github.com>
2023-01-04 15:22:54 +0100

    Delete PROJETPHOTO directory

--- PROJETPHOTO/html/Javascript/popup.js
+++
@@ -1,6 +0,0 @@
-window.onload = function toggle(){
-	var blur = document.getElementById('blur');
-	blur.classList.toggle('active');
-	var popup = document.getElementById('popup');
-	popup.classList.toggle('active');
-}
