88bed1b2f55c2cf0dbfb3b53bed1372f6217a341
LudovicUrbes <68041508+LudovicUrbes@users.noreply.github.com>
2023-01-07 16:04:23 +0100

    Delete concours directory

--- concours/admin/components/customPasswordField.php
+++
@@ -1,7 +0,0 @@
-<div class="relative w-full">
-    <div class="absolute inset-y-0 right-0 flex items-center px-2">
-        <input class="hidden js-password-toggle" id="toggle" type="checkbox" />
-        <label class="bg-gray-300 hover:bg-gray-400 rounded px-2 py-1 text-sm text-gray-600 font-mono cursor-pointer js-password-label mb-4" for="toggle">Show</label>
-    </div>
-    <input class="appearance-none mt-1 w-full outline-none border-b-2 border-gray-300 text-base pr-16 js-password" name="password" id="password" type="password" autocomplete="off" required />
-</div>
--- concours/admin/controllers/logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-session_start();
-session_destroy();
-header('Location: /SAE-302/concours/index.php');
